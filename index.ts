a12205fda3af496988ec503316951c832fc13b27
黄益望 <13906676604@100fen.cn>
2019-11-04 14:02:59 +0800

    pc?

--- index.ts
+++ index.ts
@@ -1,5 +1,33 @@
 export default function init(initWidth: number = 375) {
   clear();
+  if (ismobile()) {
+    vw(initWidth)
+  } else {
+    trs(initWidth)
+  }
+}
+
+function clear(): void {
+  let meta: NodeListOf<HTMLMetaElement> = document.querySelectorAll('meta[name="viewport"]'),
+      length: number = meta.length;
+  for (let i = length - 1; i >= 0; i--) {
+    document.head.removeChild(meta[i])
+  }
+}
+
+init();
+
+
+function ismobile(): boolean {
+  let mobileArry: string[] = ["iPhone", "iPad", "Android", "Windows Phone", "BB10; Touch", "BB10; Touch", "PlayBook", "Nokia"];
+  let ua = navigator.userAgent;
+  let res = mobileArry.filter(function (arr) {
+    return ua.indexOf(arr) > 0;
+  });
+  return res.length > 0;
+}
+
+function vw(initWidth: number = 375): void {
   let curWidth: number = window.screen.width;
   let scale: number = curWidth / initWidth;
   let meta: HTMLMetaElement = document.createElement("meta");
@@ -8,11 +36,14 @@ export default function init(initWidth: number = 375) {
   document.head.appendChild(meta);
 }
 
-function clear() {
-  let meta = document.querySelectorAll('meta[name="viewport"]'), length = meta.length;
-  for (let i = length - 1; i >= 0; i--) {
-    document.head.removeChild(meta[i])
-  }
+function trs(initWidth: number = 375): void {
+  let meta: HTMLMetaElement = document.createElement("meta");
+  meta.name = "viewport";
+  meta.content = `width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0`;
+  document.head.appendChild(meta);
+  let body: HTMLBodyElement = document.getElementsByTagName('body')[0]
+  body.style.width = initWidth + 'px';
+  let scale: number = document.documentElement.clientWidth / initWidth;
+  body.style.transform = `scale(${scale})`
+  body.style.transformOrigin = '0 0'
 }
-
-init();
